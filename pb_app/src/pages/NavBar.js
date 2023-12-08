03d0bc39aca05fc98d3eda2677b75b7fc3996b09
tvirata <45518488+tvirata@users.noreply.github.com>
2023-12-08 14:30:12 -0800

    editing navbar styles

--- pb_app/src/pages/NavBar.js
+++ pb_app/src/pages/NavBar.js
@@ -20,27 +20,38 @@ export default function NavBar() {
   if (pb.authStore.isValid) {
     return (
       <header className="header">
-      <div class="topnav">
-        <a href="/"> Home </a>
-        <a href="history"> Expense Tracker </a>
-        <a href="about"> About Us </a>
-        <a style={{marginLeft:'65%'}} className="logoutbutt" href="/" onClick={logout}> Logout <i class="fa-solid fa-right-from-bracket"></i></a>
+        <div class="topnav">
+          <a href="/"> Home </a>
+          <a href="history"> Expense Tracker </a>
+          <a href="about"> About Us </a>
+          <div className="blank"></div>
+          <a
+            // style={{ marginLeft: "65%" }}
+            className="logoutbutt"
+            href="/"
+            onClick={logout}
+          >
+            {" "}
+            Logout <i class="fa-solid fa-right-from-bracket"></i>
+          </a>
         </div>
       </header>
     );
   } else {
     return (
-      
       <header className="header">
-      <div class="topnav">
-        <a href="/"> Home </a>
-        <a href="history"> Expense Tracker </a>
-        <a href="about"> About Us </a>
-        <a style={{marginLeft:'60%'}} href="login"> Log In </a>
-        <a style={{marginLeft:'0'}} className="regbutt" href="signup"> Sign Up </a>
+        <div class="topnav">
+          <a href="/"> Home </a>
+          <a href="history"> Expense Tracker </a>
+          <a href="about"> About Us </a>
+          <div className="blank"></div>
+          <a href="login"> Log In </a>
+          <a style={{ marginLeft: "0" }} className="regbutt" href="signup">
+            {" "}
+            Sign Up{" "}
+          </a>
         </div>
       </header>
-
     );
   }
 }
